3a028de10f4ef85a65c933e3d13149e7c6538995
Nebiyou Yonas Metaferia <nebsdivo@gmail.com>
2022-11-12 14:05:10 -0800

    sorting_algo: wrapped up insertion sort

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts doubly linked list using insertion sort algo
+ * @list: pointer to a doubly linked list pointer
+ *
+ * Return: void (no Return)
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *head = *list, *prev;
+
+	if (!list || !(*list) || !((*list)->next))
+		return;
+
+	while (head)
+	{
+		if (head->prev)
+		{
+			prev = head->prev;
+			if (head->n < prev->n)
+			{
+				prev->next = head->next;
+				if (prev->next)
+					prev->next->prev = prev;
+				head->next = prev;
+				head->prev = prev->prev;
+				prev->prev = head;
+				if (head->prev)
+					head->prev->next = head;
+				else
+					*list = head;
+				print_list(*list);
+				continue;
+			}
+		}
+		head = head->next;
+	}
+}
